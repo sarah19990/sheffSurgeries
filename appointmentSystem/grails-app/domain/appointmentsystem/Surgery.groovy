3a8bf2b46237285eb8a56f50a36179b472d20bba
sarah19990 <sara1999200407@gmail.com>
2022-02-26 15:10:28 +0000

    created controllers and views to SurgeryController.groovy

--- appointmentSystem/grails-app/domain/appointmentsystem/Surgery.groovy
+++ appointmentSystem/grails-app/domain/appointmentsystem/Surgery.groovy
@@ -1,6 +1,13 @@
 package appointmentsystem
 
 class Surgery {
+   String name
+   String address
+   String postcode
+   String telephone
+   int numberOfPatients
+   String description
+   Date openingtime
 
     static constraints = {
     }
