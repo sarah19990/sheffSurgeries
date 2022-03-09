538504c0089cc80de70e1a087fc296612f0f09e7
sarah19990 <sara1999200407@gmail.com>
2022-03-09 14:49:00 +0000

    Added Constraints and datatype to Receptionist.groovy

--- appointmentSystem/grails-app/domain/com/sali/Receptionist.groovy
+++ appointmentSystem/grails-app/domain/com/sali/Receptionist.groovy
@@ -1,7 +1,17 @@
 package com.sali
 
 class Receptionist {
+   String recepName
+   String recepEmail
+   String recepUsername
+   String recepPassword
+   String recepPhone
 
     static constraints = {
+       recepName blank:false
+       recepEmail blank:false, email: true
+       recepUsername blank:false, unique: true
+       recepPassword blank:false
+       recepPhone blank:false 
     }
 }
