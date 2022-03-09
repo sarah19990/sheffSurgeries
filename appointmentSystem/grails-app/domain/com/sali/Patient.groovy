ccce56cd7fd03b053bf1385d84276fe281fa121e
sarah19990 <sara1999200407@gmail.com>
2022-03-09 15:00:16 +0000

    Added Constraints and datatype to Patient.groovy

--- appointmentSystem/grails-app/domain/com/sali/Patient.groovy
+++ appointmentSystem/grails-app/domain/com/sali/Patient.groovy
@@ -1,7 +1,22 @@
 package com.sali
 
 class Patient {
+   String patientName
+   String patientAddress
+   String patientResidence
+   Date patientDob
+   String patientID
+   Date dateRegistered
+   String patientPhone
 
     static constraints = {
+       patientName blank:false
+       patientAddress blank:false
+       patientResidence blank:false
+       patientDob blank:false, date: true, nullable: false
+       patientID blank: false
+       dateRegistered blank:false, nullable:false
+       patientPhone blank:false 
     }
+
 }
