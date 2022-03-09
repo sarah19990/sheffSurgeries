5e2914a166a5856e9a9c0c8f0ea1747a1fffe732
sarah19990 <sara1999200407@gmail.com>
2022-03-09 11:14:17 +0000

    Added Constraints and datatype to Doctor.groovy

--- appointmentSystem/grails-app/domain/com/sali/Doctor.groovy
+++ appointmentSystem/grails-app/domain/com/sali/Doctor.groovy
@@ -1,7 +1,23 @@
 package com.sali
 
 class Doctor {
+   String fullName
+   String qualifications
+   String position
+   String doctorEmail
+   String password
+   String doctorOffice
+   String doctorPhone
+   String bio
 
     static constraints = {
+        fullName blank:false
+        qualifications  blank:false
+        position blank:false
+        doctorEmail blank:false, email: true
+        password blank:false
+        doctorOffice blank:false
+        doctorPhone blank:false
+        bio blank:false, widget: 'textarea'
     }
 }
