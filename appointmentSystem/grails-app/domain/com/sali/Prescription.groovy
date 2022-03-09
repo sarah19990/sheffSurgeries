9581b5c20fbad5f04bb19bea7f7098430f9c5c4d
sarah19990 <sara1999200407@gmail.com>
2022-03-09 14:33:46 +0000

    Added Constraints and datatype to Prescription.groovy

--- appointmentSystem/grails-app/domain/com/sali/Prescription.groovy
+++ appointmentSystem/grails-app/domain/com/sali/Prescription.groovy
@@ -1,7 +1,19 @@
 package com.sali
 
 class Prescription {
+   String pharmacyName
+   String prescripNumber
+   String medicine
+   String totalCost
+   Date dateIssued
+   Boolean patientPaying
 
     static constraints = {
+       pharmacyName blank:false
+       prescripNumber blank:false
+       medicine blank:false
+       totalCost blank:false
+       dateIssued blank:false, nullable:false
+       patientPaying blank:false, nullable:false
     }
 }
