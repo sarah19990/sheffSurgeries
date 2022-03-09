5af745986e5491cac72e6acfe2b679beb5877b2c
sarah19990 <sara1999200407@gmail.com>
2022-03-09 11:06:42 +0000

    Added Constraints and datatype to Surgery.groovy

--- appointmentSystem/grails-app/domain/com/sali/Surgery.groovy
+++ appointmentSystem/grails-app/domain/com/sali/Surgery.groovy
@@ -1,7 +1,20 @@
 package com.sali
 
 class Surgery {
+   String name
+   String address
+   String postcode
+   String telephone
+   int numberOfPatients
+   String description
+   String openingtime
 
     static constraints = {
+        name blank:false
+       address blank:false
+       telephone blank:false
+       numberOfPatients max:50, nullable:false
+       description blank:false, widget: 'textarea'
+       openingtime blank:false
     }
 }
